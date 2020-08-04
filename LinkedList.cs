f3b4658453d763e252d6a86d1e131a1a1b3c6110
Shriya nair <shriya.nair@zensar.com>
2020-08-04 23:53:29 +0530

    Add the assignment files

---
+++ LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListAndList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> lList = new LinkedList<string>(new string[] {"quick","fox","the","lazy"});
+            //a.AddFirst()
+            //b.AddLast()
+            //c.AddAfter()
+            //d.AddBefore()
+            //e.Find()
+            //f.FindLast()
+        
+
+            
+            lList.AddFirst("the");
+            LinkedListNode<String> temp = lList.Find("quick");
+            lList.AddAfter(temp,"brown");
+            LinkedListNode<String> temp1 = lList.FindLast("the");
+            lList.AddBefore(temp1, "over");
+            LinkedListNode<String> temp2 = lList.FindLast("over");
+            lList.AddBefore(temp2, "jumps");
+            lList.AddLast("fox");
+            
+         
+            foreach (var i in lList)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
